e11c3cb5ab554946b2a9b376b37a57087d5beeb6
陈广亮 <guangliang.chen@datayes.com>
2016-12-02 20:10:05 +0800

    fix about page

--- fe/js/apps/about/index.js
+++ fe/js/apps/about/index.js
@@ -5,7 +5,7 @@ export default class About extends Component {
     render() {
         return (
             <div className='aboutPage'>
-                {"本网站为学习nodejs创建，正在不停完善中 项目地址 https://github.com/geraldchen890806/blog_nodejs"}
+                {"本网站为学习nodejs+react创建，正在不停完善中 项目地址 https://github.com/geraldchen890806/blog"}
             </div>
         );
 
