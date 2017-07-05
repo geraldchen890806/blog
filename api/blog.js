e0da99b158c4424196ff30d296ad93d898396373
geraldchen <geraldchen890806@gmail.com>
2017-07-05 18:16:32 +0800

    fix eslint

--- api/blog.js
+++ api/blog.js
@@ -1,25 +1,25 @@
-let DB = require('./db');
-let db = new DB('blogs');
-let mm = require('moment');
+var DB = require("./db");
+var db = new DB("blogs");
+var mm = require("moment");
 
-exports.sqlBlogs = function (callback) {
+exports.sqlBlogs = function(callback) {
   // var blogs = this.queryStr("SELECT * FROM blogs left join (select blog_tag.blogID,blog_tag.tagID,name as tagName from tags left join blog_tag on tags.id = blog_tag.tagID ) b on blogs.id = b.blogID order by blogs.addTime DESC");
   db.queryStr(
-    'SELECT * FROM blogs left join (select blog_tag.blogID,blog_tag.tagID,name as tagName from tags left join blog_tag on tags.id = blog_tag.tagID ) b on blogs.id = b.blogID order by blogs.addTime DESC',
+    "SELECT * FROM blogs left join (select blog_tag.blogID,blog_tag.tagID,name as tagName from tags left join blog_tag on tags.id = blog_tag.tagID ) b on blogs.id = b.blogID order by blogs.addTime DESC",
     {},
-    (blogs) => {
-      let res = [];
-      blogs.forEach((v, i) => {
-        let id = parseInt(v.id);
+    function(blogs) {
+      var res = [];
+      blogs.forEach(function(v, i) {
+        var id = parseInt(v.id);
         if (res[id]) {
           res[id].tags.push({
             id: v.tagID,
             name: v.tagName
           });
         } else {
-          let date = mm(v.addTime);
+          var date = mm(v.addTime);
           v.pubDate = date.format();
-          v.addTime = date.format('LL');
+          v.addTime = date.format("LL");
           v.tags = [];
           if (v.tagID) {
             v.tags.push({
@@ -32,7 +32,9 @@ exports.sqlBlogs = function (callback) {
         // console.log(res[id]);
       });
       res = res
-        .filter((v, i) => !!v)
+        .filter(function(v, i) {
+          return !!v;
+        })
         .reverse();
       callback(res);
     }
