dbd9a42975a3f49a3948226da7d1fab3ccbcfae0
geraldchen <geraldchen890806@gmail.com>
2017-09-06 19:24:48 +0800

    test wx

--- fe/js/utils/wxShare.js
+++ fe/js/utils/wxShare.js
@@ -32,15 +32,15 @@ function wxConfig(options) {
     jsApiList: ['onMenuShareAppMessage', 'onMenuShareTimeline', 'onMenuShareQQ', 'onMenuShareWeibo'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
   });
   wx.ready(() => {
-    alert('ready');
-    // 分享给朋友
-    wx.onMenuShareAppMessage(options.obj);
-    // 分享到朋友圈
-    wx.onMenuShareTimeline(options.obj);
-    // 分享到QQ
-    wx.onMenuShareQQ(options.obj);
-    // 分享到腾讯微博
-    wx.onMenuShareWeibo(options.obj);
+    // alert('ready');
+    // // 分享给朋友
+    // wx.onMenuShareAppMessage(options.obj);
+    // // 分享到朋友圈
+    // wx.onMenuShareTimeline(options.obj);
+    // // 分享到QQ
+    // wx.onMenuShareQQ(options.obj);
+    // // 分享到腾讯微博
+    // wx.onMenuShareWeibo(options.obj);
   });
   wx.error((res) => {
     console.log(res);
@@ -48,7 +48,25 @@ function wxConfig(options) {
 }
 
 wx.ready(() => {
-  alert('ready2');
+  wx.onMenuShareAppMessage({
+    title: '互联网之子',
+    desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
+    link: 'http://movie.douban.com/subject/25785114/',
+    imgUrl: 'http://img3.douban.com/view/movie_poster_cover/spst/public/p2166127561.jpg',
+    trigger(res) {
+      alert('用户点击发送给朋友');
+    },
+    success(res) {
+      alert('已分享');
+    },
+    cancel(res) {
+      alert('已取消');
+    },
+    fail(res) {
+      alert(JSON.stringify(res));
+    }
+  });
+  // alert('ready2');
   // 分享给朋友
   // wx.onMenuShareAppMessage(options.obj);
   // // 分享到朋友圈
