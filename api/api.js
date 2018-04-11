9585acd3f61a2b46333482c75745ddee6c41ec98
guangliang.chen <geraldchen890806@gmail.com>
2018-04-11 22:21:44 +0800

    fix style

--- api/api.js
+++ api/api.js
@@ -1,31 +1,31 @@
-var express = require("express");
-var blog = require("./blog");
-const request = require("superagent");
-var _ = require("lodash");
+const express = require('express');
+const blog = require('./blog');
+const request = require('superagent');
+const _ = require('lodash');
 
-const secret = "3f4c554304b13b6d8d3229dffc17a6f0";
-var sign = require("./sign.js");
+const secret = '3f4c554304b13b6d8d3229dffc17a6f0';
+const sign = require('./sign.js');
 
 let ticket;
 
-module.exports = function(app) {
-  var allowCrossDomain = function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
+module.exports = function (app) {
+  const allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
 
     next();
   };
   app.use(allowCrossDomain);
 
-  app.all("/api/*", function(req, res, next) {
-    blog.sqlBlogs(function(data) {
+  app.all('/api/*', (req, res, next) => {
+    blog.sqlBlogs((data) => {
       res.send(data);
     });
   });
 
-  app.get("/wx/token", function(req, res, next) {
-    let query = req.query || {};
+  app.get('/wx/token', (req, res, next) => {
+    const query = req.query || {};
     console.log(query);
     if (
       ticket &&
@@ -54,7 +54,7 @@ module.exports = function(app) {
             ticket = _.assignIn(resp2.body, { time: new Date() }, query);
             res.send(
               _.assignIn(sign(ticket.ticket, query.url), ticket, {
-                url: query.url
+                url: query.url,
               })
             );
           });
--- api/blog.js
+++ api/blog.js
@@ -1,30 +1,30 @@
-var DB = require("./db");
-var db = new DB("blogs");
-var mm = require("moment");
+const DB = require('./db');
+const db = new DB('blogs');
+const mm = require('moment');
 
-exports.sqlBlogs = function(callback) {
+exports.sqlBlogs = function (callback) {
   // var blogs = this.queryStr("SELECT * FROM blogs left join (select blog_tag.blogID,blog_tag.tagID,name as tagName from tags left join blog_tag on tags.id = blog_tag.tagID ) b on blogs.id = b.blogID order by blogs.addTime DESC");
   db.queryStr(
-    "SELECT * FROM blogs left join (select blog_tag.blogID,blog_tag.tagID,name as tagName from tags left join blog_tag on tags.id = blog_tag.tagID ) b on blogs.id = b.blogID order by blogs.addTime DESC",
+    'SELECT * FROM blogs left join (select blog_tag.blogID,blog_tag.tagID,name as tagName from tags left join blog_tag on tags.id = blog_tag.tagID ) b on blogs.id = b.blogID order by blogs.addTime DESC',
     {},
-    function(blogs) {
-      var res = [];
-      blogs.forEach(function(v, i) {
-        var id = parseInt(v.id);
+    (blogs) => {
+      let res = [];
+      blogs.forEach((v, i) => {
+        const id = parseInt(v.id);
         if (res[id]) {
           res[id].tags.push({
             id: v.tagID,
-            name: v.tagName
+            name: v.tagName,
           });
         } else {
-          var date = mm(v.addTime);
+          const date = mm(v.addTime);
           v.pubDate = date.format();
-          v.addTime = date.format("LL");
+          v.addTime = date.format('LL');
           v.tags = [];
           if (v.tagID) {
             v.tags.push({
               id: v.tagID,
-              name: v.tagName
+              name: v.tagName,
             });
           }
           res[id] = v;
@@ -32,9 +32,7 @@ exports.sqlBlogs = function(callback) {
         // console.log(res[id]);
       });
       res = res
-        .filter(function(v, i) {
-          return !!v;
-        })
+        .filter((v, i) => !!v)
         .reverse();
       callback(res);
     }
