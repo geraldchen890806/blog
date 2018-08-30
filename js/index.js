ae66549d89ec9e9249146ad5e04d44b01b6a0098
陈广亮 <guangliang.chen@datayes.com>
2018-08-30 16:17:25 +0800

    test

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
-import "babel-polyfill";
-import React from "react";
-import { render } from "react-dom";
-import { Provider } from "react-redux";
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 
-import "./configs/index";
-import "../style/main.less";
-import { store } from "./redux/store";
+import './configs/index';
+import '../style/main.less';
 
-import "github-markdown-css/github-markdown.css";
+import 'github-markdown-css/github-markdown.css';
 
-import App from "js/containers";
+import App from 'js/containers';
 
 // Install ServiceWorker and AppCache in the end since
 // it's not most important operation and if main code fails,
 // we do not want it installed
-import runtime from "offline-plugin/runtime";
+import runtime from 'offline-plugin/runtime';
+import { store } from './redux/store';
 
 if (module.hot) {
   module.hot.accept();
@@ -23,27 +23,27 @@ render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   runtime.install({
     // eslint-disable-line global-require
     onUpdating: () => {
-      console.log("SW Event:", "onUpdating");
+      console.log('SW Event:', 'onUpdating');
     },
     onUpdateReady: () => {
-      console.log("SW Event:", "onUpdateReady");
+      console.log('SW Event:', 'onUpdateReady');
       // Tells to new SW to take control immediately
       runtime.applyUpdate();
     },
     onUpdated: () => {
-      console.log("SW Event:", "onUpdated");
+      console.log('SW Event:', 'onUpdated');
       // Reload the webpage to load into the new version
       // window.location.reload();
     },
 
     onUpdateFailed: () => {
-      console.log("SW Event:", "onUpdateFailed");
-    }
+      console.log('SW Event:', 'onUpdateFailed');
+    },
   });
 }
