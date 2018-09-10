1af93f57f864b2498f0eccddb56a6e35ee674708
陈广亮 <guangliang.chen@datayes.com>
2018-09-10 20:05:51 +0800

    fix webpack config

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,163 +1,165 @@
 /* eslint-disable */
-var path = require("path");
-var webpack = require("webpack");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-var CopyWebpackPlugin = require("copy-webpack-plugin");
-var OfflinePlugin = require("offline-plugin");
-var MiniCssExtractPlugin = require("mini-css-extract-plugin");
-var OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var OfflinePlugin = require('offline-plugin');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-  mode: "production",
+  mode: 'production',
   entry: {
-    main: "./js/index"
+    main: './js/index',
   },
   output: {
-    filename: "main.[chunkHash].js",
-    path: path.join(__dirname, "/static"),
-    chunkFilename: "[name].[chunkhash].chunk.js",
-    publicPath: "/"
+    filename: 'main.[chunkhash].js',
+    path: path.join(__dirname, '/static'),
+    chunkFilename: '[name].[chunkhash].chunk.js',
+    publicPath: '/',
   },
   resolve: {
-    modules: ["node_modules"],
+    modules: ['node_modules'],
     alias: {
       business: process.cwd(),
-      js: path.resolve("js"),
-      img: path.resolve("img"),
-      resources: path.resolve("js/resources"),
-      apps: path.resolve("js/apps")
+      js: path.resolve('js'),
+      img: path.resolve('img'),
+      resources: path.resolve('js/resources'),
+      apps: path.resolve('js/apps'),
     },
-    extensions: [".js"]
+    extensions: ['.js'],
   },
   optimization: {
+    runtimeChunk: 'single',
     splitChunks: {
-      chunks: "async",
+      chunks: 'async',
       minSize: 30000,
       minChunks: 1,
       maxAsyncRequests: 5,
       maxInitialRequests: 3,
-      automaticNameDelimiter: "~",
+      automaticNameDelimiter: '~',
       name: true,
       cacheGroups: {
         vendors: {
           test: /[\\/]node_modules[\\/]/,
-          priority: -10
+          priority: -10,
         },
         default: {
           minChunks: 2,
           priority: -20,
-          reuseExistingChunk: true
-        }
-      }
-    }
+          reuseExistingChunk: true,
+        },
+      },
+    },
   },
   plugins: [
     // new BundleAnalyzerPlugin(),
+    new webpack.HashedModuleIdsPlugin(),
 
     new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
-      chunkFilename: "[id].[contenthash].css"
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].[contenthash].css',
     }),
     new OptimizeCssAssetsPlugin({
       assetNameRegExp: /\.optimize\.css$/g,
-      cssProcessor: require("cssnano"),
+      cssProcessor: require('cssnano'),
       cssProcessorOptions: { safe: true, discardComments: { removeAll: true } },
-      canPrint: true
+      canPrint: true,
     }),
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "index.html"
+      filename: 'index.html',
+      template: 'index.html',
     }),
     new CopyWebpackPlugin([
       {
-        from: "img",
-        to: "img"
+        from: 'img',
+        to: 'img',
       },
       {
-        from: "manifest.json"
-      }
+        from: 'manifest.json',
+      },
     ]),
     new webpack.DefinePlugin({
       __DEVELOPMENT__: false,
       __DEVTOOLS__: false,
-      "process.env": {
-        NODE_ENV: JSON.stringify("production")
-      }
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
     }),
     new OfflinePlugin({
       relativePaths: false,
-      publicPath: "/",
+      publicPath: '/',
       ServiceWorker: {
-        events: true
+        events: true,
       },
       caches: {
-        main: [":rest:"],
+        main: [':rest:'],
 
-        additional: ["*.chunk.js"]
+        additional: ['*.chunk.js'],
       },
-      AppCache: false
-    })
+      AppCache: false,
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.js$/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
             plugins: [
               [
-                "import",
+                'import',
                 {
-                  libraryName: "antd",
-                  style: true
-                }
-              ]
-            ]
-          }
+                  libraryName: 'antd',
+                  style: true,
+                },
+              ],
+            ],
+          },
         },
         exclude: /node_modules/,
-        include: path.join(__dirname, "js")
+        include: path.join(__dirname, 'js'),
       },
       {
         test: /\.css$/,
         use: [
           {
-            loader: MiniCssExtractPlugin.loader
+            loader: MiniCssExtractPlugin.loader,
           },
-          "css-loader"
+          'css-loader',
         ],
-        include: __dirname
+        include: __dirname,
       },
       {
         test: /\.less$/,
         use: [
           {
-            loader: MiniCssExtractPlugin.loader
+            loader: MiniCssExtractPlugin.loader,
           },
-          "css-loader",
-          { loader: "less-loader", options: { javascriptEnabled: true } }
+          'css-loader',
+          { loader: 'less-loader', options: { javascriptEnabled: true } },
         ],
-        include: __dirname
+        include: __dirname,
       },
       {
         test: /\.(jpeg|png|jpg|gif|pdf|mp3|ogg|wav)$/,
-        use: ["file-loader?name=[path][name].[ext]"]
+        use: ['file-loader?name=[path][name].[ext]'],
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: ["url-loader?limit=10000&mimetype=application/font-woff"]
+        use: ['url-loader?limit=10000&mimetype=application/font-woff'],
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: ["file-loader"]
+        use: ['file-loader'],
       },
       {
         test: /\.md$/,
-        loader: ["raw-loader"]
-      }
-    ]
-  }
+        loader: ['raw-loader'],
+      },
+    ],
+  },
 };
