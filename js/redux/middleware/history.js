05fb550f56bf69f5ea0f5b7d88732a32ad8c4448
陈广亮 <guangliang.chen@datayes.com>
2018-10-11 16:01:30 +0800

    remove console

--- js/redux/middleware/history.js
+++ js/redux/middleware/history.js
@@ -1,7 +1,7 @@
 import createHistory from 'history/createBrowserHistory';
 const history = createHistory();
-history.listen((location, action) => {
+history.listen(() => {
   // location is an object like window.location
-  console.log(action, location.pathname, location.state);
+  // console.log(action, location.pathname, location.state);
 });
 export default history;
