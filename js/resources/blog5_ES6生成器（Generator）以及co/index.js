7b72f46e7ad31ba55bd569b539ca67d8df9426bb
陈广亮 <guangliang.chen@datayes.com>
2019-02-16 16:42:36 +0800

    fix

--- js/resources/blog5_ES6生成器（Generator）以及co/index.js
+++ js/resources/blog5_ES6生成器（Generator）以及co/index.js
@@ -2,7 +2,7 @@
 export default {
   title: 'ES6生成器（Generator）以及co',
   url: 'ES6生成器(Generator)以及co',
-  tags: ['node', 'koa', 'ES6'],
+  tags: ['node', 'koa', 'es6'],
   date: 'October 22, 2014',
   load: () => import(/* webpackChunkName: "blog5_ES6生成器（Generator）以及co" */ './view'),
 };
