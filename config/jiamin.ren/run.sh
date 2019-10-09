916b5de10083b1765ded6e246286e4e90f57d2e0
师狮 <guangliang.chen@andui.com>
2019-10-09 16:09:35 +0800

    fix configs

--- config/jiamin.ren/run.sh
+++ config/jiamin.ren/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cp /root/blog/config/nginx.conf /etc/nginx/sites-enabled/default
+cp /root/blog/config/jiamin.ren/nginx.conf /etc/nginx/sites-enabled/default
 cp -r /root/blog/build/* /usr/share/nginx/html/
 
 nginx
--- config/renjiamin.com/run.sh
+++ config/renjiamin.com/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cp /root/blog/config/nginx.conf /etc/nginx/sites-enabled/default
+cp /root/blog/config/renjiamin.com/nginx.conf /etc/nginx/sites-enabled/default
 cp -r /root/blog/build/* /usr/share/nginx/html/
 
 nginx
